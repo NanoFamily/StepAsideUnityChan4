b4cf82e5e9674e976f69ace0566d9f874f52c6cd
Hitoki <diavolos1230@gmail.com>
2019-10-24 21:28:48 +0900

    Lesson6の課題
    課題完了しました。提出します。

---
+++ Assets/DestroyObjectScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectScript : MonoBehaviour
+{
+    //カメラのオブジェクト
+    private GameObject maincamera;
+    //カメラとアイテムの距離
+    private float difference;
+
+    // Use this for initialization
+    void Start()
+    {
+        //カメラのオブジェクトを取得
+        this.maincamera = GameObject.Find("Main Camera");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            //カメラとアイテム (z座標)の差を求める
+            this.difference = maincamera.transform.position.z - this.transform.position.z;
+
+            if (this.difference > 0)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+
+
